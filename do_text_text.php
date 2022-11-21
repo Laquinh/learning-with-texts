2ad52f2f661b3cd5d76c074627b28fd7de095647
Laquin-Laquih <anderaginaga2@gmail.com>
2022-11-22 00:37:26 +0100

    remove null texts

--- do_text_text.php
+++ do_text_text.php
@@ -86,8 +86,8 @@ $(document).ready( function() {
 </script>
 <?php
 
-echo '<div id="thetext" ' .  ($rtlScript ? 'dir="rtl"' : '') . '><p style="' . ($removeSpaces ? 'word-break:break-all;' : '') . 
-'font-size:' . $textsize . '%;line-height: 1.4; margin-bottom: 10px;">';
+echo '<div id="thetext" class="justify" ' .  ($rtlScript ? 'dir="rtl"' : '') . '><p style="' . ($removeSpaces ? 'word-break:break-all;' : '') . 
+'font-size:' . $textsize . '%;line-height: 1.6; margin-bottom: 10px; font-family:\'Meiryo\'">';
 
 $currcharcount = 0;
 
@@ -95,7 +95,7 @@ $items = textItemList($_REQUEST["text"]);
 $wordsInDB = databaseWordList($langid);
 
 //Main loop
-$showNextSpace = true;
+$showNextSpace = false;
 $whitespaceHTML = ($showNextSpace ? " " : '<span class="whitespace" style="font-size:0"> </span>');
 
 for($i = 0, $wordIndex = 0; $i < count($items); ++$i)
